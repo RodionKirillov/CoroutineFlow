94328b8905df2794f4d8b3abaf64e792924d0261
RodionKirillov <rodion.44454@gmail.com>
2024-07-21 16:06:07 +0300

    15.7 Using Flow on a Ui Layer

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,30 +4,32 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
+    val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
         .onStart {
             log("onStart")
             emit(State.Loading)
         }
+        .onEach { log("onEach") }
         .onCompletion {
             log("onCompletion")
         }
-        .asLiveData()
+
 
     private fun log(message: String) {
         Log.d(LOG_TAG, message)
-
     }
 
     companion object {
