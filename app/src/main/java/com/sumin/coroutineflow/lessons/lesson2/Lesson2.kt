2e46de4dd83d97aefb7f1f8238f44f744aa115bc
RodionKirillov <rodion.44454@gmail.com>
2024-07-21 12:27:23 +0300

    add lesson 2

--- app/src/main/java/com/sumin/coroutineflow/lessons/lesson2/Lesson2.kt
+++ app/src/main/java/com/sumin/coroutineflow/lessons/lesson2/Lesson2.kt
@@ -1,19 +1,35 @@
 package com.sumin.coroutineflow.lessons.lesson2
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 
 suspend fun main() {
-    val numbers = listOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61).asFlow()
-    numbers.filter { it.isPrime() }
+
+    getFlowByBuilderFlow().filter { it.isPrime() }
         .filter { it > 20 }
         .map {
             println("Map")
             "Number: $it"
         }
         .collect { println(it) }
+}
+
+fun getFlowByFlowOfBuilder(): Flow<Int> {
+    return flowOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+}
+
+fun getFlowByBuilderFlow(): Flow<Int> {
+    val numbers = listOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+    return flow {
+        numbers.forEach {
+            emit(it)
+        }
+    }
 }
 
 suspend fun Int.isPrime(): Boolean {
