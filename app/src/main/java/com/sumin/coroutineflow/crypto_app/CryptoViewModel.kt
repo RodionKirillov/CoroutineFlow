b5a523af104520ab87bb67c78d38979ab3ec023e
RodionKirillov <rodion.44454@gmail.com>
2024-07-24 14:42:27 +0300

    15.10 MutableSharedFlow

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
 
@@ -18,15 +20,13 @@ class CryptoViewModel : ViewModel() {
     val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
-        .onStart {
-            log("onStart")
-            emit(State.Loading)
-        }
-        .onEach { log("onEach") }
-        .onCompletion {
-            log("onCompletion")
-        }
+        .onStart { emit(State.Loading) }
 
+    fun refreshList() {
+        viewModelScope.launch {
+            repository.refreshList()
+        }
+    }
 
     private fun log(message: String) {
         Log.d(LOG_TAG, message)
