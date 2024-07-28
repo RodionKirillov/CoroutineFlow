fda5c635b26195dd6cdc3858020b6b82387bcbe3
RodionKirillov <rodion.44454@gmail.com>
2024-07-28 15:04:22 +0300

    15.15 ShareIn and StateIn

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoActivity.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoActivity.kt
@@ -3,6 +3,7 @@ package com.sumin.coroutineflow.crypto_app
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
@@ -59,6 +60,22 @@ class CryptoActivity : AppCompatActivity() {
                                 binding.progressBarLoading.isVisible = false
                                 binding.buttonRefreshList.isEnabled = true
                                 adapter.submitList(it.currencyList)
+                            }
+                        }
+                    }
+            }
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.state2
+                    .collect {
+                        when (it) {
+                            is State.Content -> {
+                                Log.d("LOG_TAG", it.currencyList.toString())
+                            }
+
+                            else -> {
+
                             }
                         }
                     }
