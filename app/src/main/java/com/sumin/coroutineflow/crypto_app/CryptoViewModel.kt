ae573ed552b98130415155358a48fde35a9e7579
RodionKirillov <rodion.44454@gmail.com>
2024-11-08 18:03:35 +0300

    15.7

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
@@ -14,7 +15,7 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
+    val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
         .onStart {
@@ -23,5 +24,5 @@ class CryptoViewModel : ViewModel() {
         }
         .onEach { Log.d("LOG_TAG", "$it") }
         .onCompletion { Log.d("LOG_TAG", "onCompletion") }
-        .asLiveData()
+
 }
